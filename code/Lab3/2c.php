0a2aa2697b84cd47682ff99246e1d793bd377b08
Leonid <stolyar.lr@gmail.com>
2022-03-05 00:55:36 +0200

    пункт 2с

---
+++ code/Lab3/2c.php
@@ -0,0 +1,19 @@
+<?php 
+
+session_start();
+
+if (isset($_POST["name"]) && isset($_POST["age"]) && isset($_POST["salary"]) && isset($_POST["pet"])) 
+{
+    $_SESSION["mas"] = [
+        "name" => $_POST["name"],
+        "age" => $_POST["age"],
+        "salary" => $_POST["salary"],
+        "pet" => $_POST["pet"]
+    ];
+    
+    header('Location: ./2c_info.php');
+}
+else
+{   
+    echo "Error";
+}
