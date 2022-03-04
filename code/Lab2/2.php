b4196c41bc8a87864416f21655bb2c91626295d6
Leonid <stolyar.lr@gmail.com>
2022-03-04 16:30:32 +0200

    2.php

---
+++ code/Lab2/2.php
@@ -0,0 +1,16 @@
+<?php
+
+$Number = 10;
+echo nl2br("$Number");
+echo nl2br("\n");
+
+$Number2 = 19.84;
+echo nl2br("$Number2");
+echo nl2br("\n");
+
+echo nl2br($Number + 2);
+echo nl2br("\n");
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo nl2br($last_month - $this_month);
